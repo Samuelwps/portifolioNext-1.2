11fa39487887b61d2eb2964ad41238e1b918108a
Samuelwps <samuelrip230@gmail.com>
2022-09-30 16:15:10 -0300

    Adding if

--- src/pages/api/ContactMe.ts
+++ src/pages/api/ContactMe.ts
@@ -6,13 +6,14 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async(req:NextApiRequest, res:NextApiResponse) =>{
   if(req.method === "POST" ){
-  const {email,name,message} = req.body
+    console.log("leu")
+    const {email,name,message} = req.body
 
-  const client = new faunadb.Client({secret: process.env.NEXT_APP_FAUNA_KEY})
-      await client.query(q.Create(q.Collection("contact"), {data: {name:name,email:email, message:message}}))
-      res.status(200).json({})
-    } else {
-      res.setHeader("Allow", "POST")
-      res.status(405).end("Method not allowed")
-    }
+    const client = new faunadb.Client({secret: process.env.NEXT_APP_FAUNA_KEY})
+    await client.query(q.Create(q.Collection("contact"), {data: {name:name,email:email, message:message}}))
+    res.status(200).json({})
+  } else {
+    res.setHeader("Allow", "POST")
+    res.status(405).end("Method not allowed")
+  }
 }
