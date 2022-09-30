9029865e931b3285449b7ad8b7e83556d484054d
Samuelwps <samuelrip230@gmail.com>
2022-09-30 16:13:04 -0300

    Adding if

--- src/pages/api/ContactMe.ts
+++ src/pages/api/ContactMe.ts
@@ -5,10 +5,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 
 export default async(req:NextApiRequest, res:NextApiResponse) =>{
+  if(req.method === "POST" ){
   const {email,name,message} = req.body
 
   const client = new faunadb.Client({secret: process.env.NEXT_APP_FAUNA_KEY})
-  if(req.method === "POST" ){
       await client.query(q.Create(q.Collection("contact"), {data: {name:name,email:email, message:message}}))
       res.status(200).json({})
     } else {
