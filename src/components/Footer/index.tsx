cac2122e29b0f739f6e17617f5787d5f4757a1c5
Samuelwps <samuelrip230@gmail.com>
2022-10-17 20:58:45 -0300

    Adding footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import {Container, ContentFooter} from "./styled"
+
+export function Footer(){
+  return(
+    <Container>
+      <ContentFooter>
+        <p>Samuel Ribeiro © 2022</p>
+      </ContentFooter>
+    </Container>
+    )
+  }
---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  background: var(--white-100);
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  width:100%;
+  height:5rem;
+  margin-top:2rem;
+
+`;
+
+export const ContentFooter = styled.div`
+  display:flex;
+  align-items:center;
+  justify-content:center;
+
+  p{
+    font-size: 1rem;
+  }
+`;
